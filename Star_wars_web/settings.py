6cfb4727f6a711e77c4482dd336b9380b8970b4f
Chopperkasa <limaszandresfelipe@gmail.com>
2022-12-22 17:57:49 -0500

    Se oculta la secret key

--- Star_wars_web/settings.py
+++ Star_wars_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mf^&)ii4&y(8_ajjz$w6%j)h_4_f&c+9n(=-#l#bv8sj==ct&$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
