47671a865516245f5e2d711b1f74da5df48ce999
Chopperkasa <limaszandresfelipe@gmail.com>
2022-12-28 11:46:59 -0500

    Se crea la barra de navegación

--- Star_wars_web/settings.py
+++ Star_wars_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Star_wars_app/static/Star_wars_app'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
