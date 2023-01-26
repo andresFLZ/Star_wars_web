78f13a8d9e3b0b59c5a58a3a81e8a00a0d1080cd
Chopperkasa <limaszandresfelipe@gmail.com>
2023-01-26 15:57:30 -0500

    Creación del template de pagina de inicio

--- personajes/views.py
+++ personajes/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def personajes (request):
 
-    return render(request, "personajes/personajes.html")
+    campos = ["nombre", "año nacimiento", "genero", "planeta origen", "peliculas", "especie", "naves espaciales", "vehiculos"]
+    filas = [1, 2]
+
+    return render(request, "personajes/personajes.html", {"campos" : campos, "filas" : filas, "bloques_s" : (len(campos) - ((len(campos)//3)*3))})
